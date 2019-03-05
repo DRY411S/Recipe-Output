1a3712948e5177c701da3387e241b0e1937210c5
Martin Gyde Poulsen <gyde@nqrd.dk>
2019-03-05 20:16:47 +0100

    Fixed looping issue with ingredients
    * Rewritten functions to be more generic
    * Changed output format to csv and ; instead of |
    * Fixed looping issue with only 5 output ingredients instead of 6

--- control.lua
+++ control.lua
@@ -1,69 +1,81 @@
 -- GLOBAL defines--
 
+local function join(delimiter, parts)
+
+    local output = ""
+    for _, part in pairs(parts) do
+        if (output == "") then
+            output = part
+        else
+            output = output .. delimiter .. part
+        end
+    end
+    return output
+end
+
+local function extractData(data, line, limit)
+    local prodcount = 1
+    for _,p in pairs(data) do
+        table.insert(line, p.name)
+        if p.type ~= nil then
+          table.insert(line, p.type)
+        else
+          table.insert(line, "")
+        end
+        if p.amount ~= nil then
+            table.insert(line, p.amount)
+        else
+            table.insert(line, 1)
+        end
+        prodcount = prodcount + 1
+    end
+    while prodcount <= limit do
+        table.insert(line, "")
+        table.insert(line, "")
+        table.insert(line, "")
+        prodcount = prodcount + 1
+    end
+    return line
+end
+
+local function logrecipe(recipe, delimiter)
+    -- Dumps the recipe into a logfile
+    line = {recipe.name, recipe.category, recipe.group.name, recipe.subgroup.name, recipe.force.name, recipe.energy}
+    line = extractData(recipe.products, line, 3)
+    line = extractData(recipe.ingredients, line, 6)
+    dataString = join(delimiter, line) .. "\n"
+
+    game.write_file("recipedump.csv", dataString, true)
+end
+
 local function logrecipes()
 
-    local output = "Name|Category|Group|Subgroup|Force|(Base) Craft Time|"
-    local prodstring, indstring
-    prodstring = ""
-    indstring = ""
-    local prodcount = 0
-    while prodcount < 6 do
-        prodcount = prodcount + 1
-        prodstring = prodstring .. "Product " .. prodcount .. " Type|Product " .. prodcount .. "|Product " .. prodcount .. " Amount|"
-        indstring = indstring .. "Ingredient " .. prodcount .. " Type|Ingredient " .. prodcount .. "|Ingredient " .. prodcount .. " Amount|"
+    local headers = {"Name","Category","Group","Subgroup","Force","(Base) Craft Time"}
+    local prodstring, indstring, delimiter
+    delimiter = ";"
+    for i = 1,3
+    do
+        table.insert(headers, "Product " .. i)
+        table.insert(headers, "Product Type " .. i)
+        table.insert(headers, "Product Amount " .. i)
     end
-    game.write_file("recipedump.txt",output .. prodstring .. indstring .. "\n")
+
+    for i = 1,6
+    do
+        table.insert(headers, "Ingredient " .. i)
+        table.insert(headers, "Ingredient Type " .. i)
+        table.insert(headers, "Ingredient Amount " .. i)
+    end
+
+    headerString = join(delimiter, headers)
+    game.remove_path("recipedump.csv")
+    game.write_file("recipedump.csv", headerString .. "\n")
 	for _, nextforce in pairs(game.forces) do
 		force = nextforce
 				for _,v in pairs(force.recipes) do
-                        logrecipe(v)
+                        logrecipe(v, delimiter)
 				end
 	end
-end
-
-function logrecipe(recipe)
-    -- Dumps the recipe into a logfile
-    local output = ""
-    local prodcount = 0
-    output = recipe.name .. "|" .. recipe.category .. "|" .. recipe.group.name .. "|" .. recipe.subgroup.name .. "|" .. recipe.force.name .. "|" .. recipe.energy .. "|"
-    for _,p in pairs(recipe.products) do
-        prodcount = prodcount + 1
-        if p.type ~= nil then
-          output = output .. p.type  .. "|"
-        else
-          output = output .. "|"
-        end
-        output = output .. p.name .. "|"
-        if p.amount ~= nil then
-            output = output .. p.amount .. "|"
-        else
-            output = output .. "1|"
-        end
-    end
-    while prodcount < 6 do
-        prodcount = prodcount + 1
-        output = output .. "|||"
-    end
-    prodcount = 0
-    for _,p in pairs(recipe.ingredients) do
-        if p.type ~= nil then
-            output = output .. p.type .. "|"
-        else
-            output = output .. "|"
-        end
-        output = output .. p.name .. "|"
-        if p.amount ~= nil then
-            output = output .. p.amount .. "|"
-        else
-            output = output .. "1|"
-        end
-    end
-    while prodcount < 6 do
-        prodcount = prodcount + 1
-        output = output .. "|||"
-    end
-    output = output .. "\n"
-    game.write_file("recipedump.txt",output,true)
 end
 
 -- LOCAL Variables
